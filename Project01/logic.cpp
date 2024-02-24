c919ee3a62a9f13547a94c55c4f9d71ea20db87e
Евгений Шкумат <kotbmasle@gmail.com>
2024-02-24 14:56:10 +0300

    add and use find_avg_arithmetical and   sum_elements_below_avg functions

--- Project01/logic.cpp
+++ Project01/logic.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 int amount_of_even_elements(int* vector, int size) {
 	int count = 0;
 
@@ -9,4 +11,28 @@ int amount_of_even_elements(int* vector, int size) {
 	}
 
 	return count;
+}
+
+double find_avg_arithmetical(int* vector, int size) {
+	double sum = 0;
+	
+	for (int i = 0; i < size; i++)
+	{
+		sum += *(vector + i);
+	}
+
+	return sum / size;
+}
+
+int sum_elements_below_avg(int* vector, int size, double avg) {
+	int sum = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (abs(*(vector + i)) < avg) {
+			sum += *(vector + i);
+		}
+	}
+
+	return sum;
 }
--- Project01/main.cpp
+++ Project01/main.cpp
@@ -5,17 +5,22 @@ string output(int* array, int size);
 void random_init(int* array, int size, int start);
 
 int amount_of_even_elements(int* vector, int size);
+int sum_elements_below_avg(int* vector, int size, double avg);
+double find_avg_arithmetical(int* vector, int size);
 
 int main() {
 	srand(time(NULL));
 
-	int size = 8;
+	int size = 10;
 	int* vector = new int[size];
 
-	random_init(vector, size, 1);
+	random_init(vector, size, 0);
 	cout << output(vector, size);
 
-	cout << "\nAmount of even numbers is " << amount_of_even_elements(vector, size);
+	double avg = find_avg_arithmetical(vector, size);
+
+	cout << "\nThe sum of elements which are less than avg arithmetic by their absolute value is: "
+		<< sum_elements_below_avg(vector, size, avg);
 
 	return 0;
 }
